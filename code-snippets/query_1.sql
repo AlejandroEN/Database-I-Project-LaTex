03709e8024e0e8e857af073e10b655bfd66bf7e6
KarTaGo124 <joseguillermogalvezpacori@gmail.com>
2024-07-01 21:18:28 -0500

    query 1 included

--- code-snippets/query_1.sql
+++ code-snippets/query_1.sql
@@ -1,39 +1,27 @@
 SELECT
-    persona.nombres || ' ' || persona.primer_apellido || ' ' || persona.segundo_apellido AS nombre_director,
-    persona.email,
-    colaborador.numero_celular,
+    d_persona.nombres || ' ' || d_persona.primer_apellido || ' ' || d_persona.segundo_apellido AS nombre_director,
+    d_persona.email AS email_director,
+    d_colaborador.numero_celular AS celular_director,
     sede.direccion,
     sede.coordenada_longitud,
-    sede.coordenada_latitud
+    sede.coordenada_latitud,
+    sede.construccion_fecha
 FROM
     director
-        JOIN colaborador ON director.dni = colaborador.dni
-        JOIN persona ON colaborador.dni = persona.dni
+        JOIN colaborador AS d_colaborador ON director.dni = d_colaborador.dni
+        JOIN persona AS d_persona ON d_colaborador.dni = d_persona.dni
         JOIN sede ON director.sede_id = sede.id
 WHERE
-    sede.id IN (
-        SELECT
-            sede.id
-        FROM (
-                 SELECT
-                     sede.id,
-                     COUNT(salon.nombre_seccion) AS numero_salones,
-                     (SELECT COUNT(matricula.alumno_dni)
-                      FROM matricula
-                      WHERE matricula.sede_id = sede.id AND matricula.year = EXTRACT(YEAR FROM CURRENT_DATE)) AS numero_alumnos
-                 FROM
-                     sede
-                         LEFT JOIN salon ON sede.id = salon.sede_id
-                 GROUP BY
-                     sede.id,
-                     sede.construccion_fecha
-                 ORDER BY
-                     numero_salones DESC,
-                     sede.construccion_fecha
-                 LIMIT 20
-             ) AS sedes_filtradas
-        WHERE sedes_filtradas.numero_alumnos <= 300
-          AND sedes_filtradas.numero_alumnos >= 200
-    )
+    sede.construccion_fecha BETWEEN '1990-01-01' AND '2010-12-31'
+  AND (
+          SELECT COUNT(*)
+          FROM profesor_sede
+          WHERE profesor_sede.sede_id = sede.id
+      ) + (
+          SELECT COUNT(*)
+          FROM alumno
+          WHERE alumno.salon_sede_id = sede.id
+      ) <= 400
 ORDER BY
-    sede.id;
+    sede.construccion_fecha
+LIMIT 20;
---
+++ code-snippets/query_1_index.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_sede_construccion_fecha ON 
+    sede (construccion_fecha);
